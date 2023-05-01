a7386e39c0303aa727963ce0074b9b985a46cfc7
eli8527 <eli8527@users.noreply.github.com>
2023-05-01 01:02:40 +0000

    deploy: 6b87ef7cb0a01f038d74a919ae7a388145e00dc8

--- scripts/main.027d261b9353c5beb196a20efdc4bc64549fdea5ad20af4c24c19ceeb8de648d5f09b1ad97bab0608cf890091de1ee918eb315d82710850453bfb31d34a22986.js
+++ scripts/main.027d261b9353c5beb196a20efdc4bc64549fdea5ad20af4c24c19ceeb8de648d5f09b1ad97bab0608cf890091de1ee918eb315d82710850453bfb31d34a22986.js
@@ -5,7 +5,12 @@
     let row = 2;
     let col = 1;
     document.querySelectorAll("main > div").forEach((el) => {
-      let w = Math.round(Math.random() * 1 + 2);
+      let minW = 2;
+      if (el.classList.contains("primary") || el.querySelector(".primary") !== null) {
+        console.log(el);
+        minW = 3;
+      }
+      let w = Math.round(Math.random() * 1 + minW);
       let offset = Math.round(Math.random() * 3 + 1);
       if (col + w + offset > columns) {
         col = offset;
